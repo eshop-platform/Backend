3f6537835bf8bc02d497a2a7c212088557ec6aa2
Selamawit <selamawitd869@gmail.com>
2026-04-22 01:59:56 +0300

    add render config

--- src/config/db.js
+++ src/config/db.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB connected ");
-  } catch (error) {
-    console.error("DB connection error:", error.message);
+  } catch (err) {
+    console.error("MongoDB error ", err.message);
     process.exit(1);
   }
 };
--- src/server.js
+++ src/server.js
@@ -1,29 +1,13 @@
 require("dotenv").config();
 
-const express = require("express");
-const app = express();
+const app = require("./app");
 const connectDB = require("./config/db");
 
-// middleware
-app.use(express.json());
-
-// routes
-app.use("/api/auth", require("./routes/auth.routes"));
-app.use("/api/users", require("./routes/user.routes"));
-app.use("/api/products", require("./routes/product.routes"));
-
-// test route
-app.get("/api/health", (req, res) => {
-  res.json({ status: "ok" });
-});
-
-// connect DB
 connectDB();
 
-//  FORCE LISTEN
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(" SERVER STARTED");
-  console.log(` http://localhost:${PORT}`);
+  console.log(`SERVER STARTED`);
+  console.log(`Running on port ${PORT}`);
 });
