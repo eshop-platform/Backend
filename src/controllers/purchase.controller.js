b1fa01d1edbc64b22eeba81f292ff2fa4655bfdd
Bereket Teshome <bereket2356@gmail.com>
2026-04-23 21:02:16 +0300

    refactor: remove approval and rejection methods from purchase controller

--- src/controllers/purchase.controller.js
+++ src/controllers/purchase.controller.js
@@ -61,34 +61,3 @@ exports.rejectPurchase = async (req, res, next) => {
 };
 
 
-exports.approvePurchase = async (req, res, next) => {
-  try {
-    const purchase = await Purchase.findByIdAndUpdate(
-      req.params.id,
-      { status: "completed" },
-      { new: true }
-    );
-    if (!purchase) {
-      return res.status(404).json({ success: false, message: "Purchase not found" });
-    }
-    res.status(200).json({ success: true, data: purchase });
-  } catch (error) {
-    next(error);
-  }
-};
-
-exports.rejectPurchase = async (req, res, next) => {
-  try {
-    const purchase = await Purchase.findByIdAndUpdate(
-      req.params.id,
-      { status: "rejected" },
-      { new: true }
-    );
-    if (!purchase) {
-      return res.status(404).json({ success: false, message: "Purchase not found" });
-    }
-    res.status(200).json({ success: true, data: purchase });
-  } catch (error) {
-    next(error);
-  }
-};
