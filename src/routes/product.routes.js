5a0991e848078c7aaa39e990024bff571dccf0e7
Bereket Teshome <bereket2356@gmail.com>
2026-04-23 20:45:25 +0300

    feat: add product retrieval and management routes

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,12 +1,26 @@
 const express = require("express");
-const { getAllProducts, approveProduct, rejectProduct, updateStock, deleteProduct } = require("../controllers/product.controller");
+const {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  approveProduct,
+  rejectProduct,
+  updateStock,
+  deleteProduct,
+} = require("../controllers/product.controller");
+const { protect } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.get("/", getAllProducts);
+router.get("/:id", getProductById);
+router.post("/", protect, createProduct);
+router.put("/:id", protect, updateProduct);
 router.patch("/:id/approve", approveProduct);
 router.patch("/:id/reject", rejectProduct);
 router.patch("/:id/stock", updateStock);
 router.delete("/:id", deleteProduct);
 
 module.exports = router;
+
